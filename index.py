7e87494131e7eb9ea1111721b5bbb9cb5eda3d70
Rohit Dalal <dalalrohit102@gmail.com>
2020-01-12 17:44:50 +0530

    Initial commit

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("home.html")
+
+@app.route("/amazon")
+def amazon():
+    return "Scrape amazon results"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
